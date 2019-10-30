4d782d1f22c268452ca9e75a8a2580ee66fd463f
Shirley-Liz <shirleymbabazi@gmail.com>
2019-10-30 13:32:15 +0300

    Add server

--- src/server/models/SignUp.js
+++ src/server/models/SignUp.js
@@ -19,12 +19,6 @@ const LoginSchema = new Schema({
   },
   PhoneNumber:{
     type: String,
-    validate: {
-        validator: function(v) {
-          return /\d{3}-\d{3}-\d{4}/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      },
       required: [true, 'User phone number required']
   },
   Password:{
